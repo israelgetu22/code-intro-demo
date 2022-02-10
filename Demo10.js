2989b1dfe216bf74568c105b0b24a125c83ee266
Isarel Getu <israelgetu101@gmail.com>
2022-02-10 13:43:29 -0600

    Donedemo

---
+++ Demo10.js
@@ -0,0 +1,11 @@
+let humburgerbackback = [];
+
+humburgerbackback.push("Bagel Sandwich");
+humburgerbackback.push("Row Boat");
+humburgerbackback.push("Mr. Gassy");
+
+console.log(humburgerbackback);
+
+humburgerbackback.splice(1, 1);
+
+let hotdogDufflebag;
