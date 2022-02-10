58e727253cef52f30b633d1ae34f1462c54e15de
Isarel Getu <israelgetu101@gmail.com>
2022-02-10 00:36:23 -0600

    Done

--- codeIntroDemo.js
+++ codeIntroDemo.js
@@ -1,60 +1,63 @@
 //////////////////////// add this later ////////////////////////
-const readline = require('readline')
+const readline = require("readline");
 
 //////////////////////// Basics ////////////////////////
-console.log('Hello, World!')
+console.log("Hello, World!");
 
-const firstName = 'Cameron'
-let age = 31
+const firstName = "Cameron";
+let age = 31;
 
-console.log(firstName, age)
+console.log(firstName, age);
 
-const isCool = true
+const isCool = true;
 
-let height
+let height;
 
-console.log(height)
+console.log(height);
 
 //////////////////////// Numbers ////////////////////////
-let x = 4 // => 4
-let y = 10 // => 10
-let z = 7 + 8 // => 15
-let summedNums = x + y + z // => 29
+let x = 4; // => 4
+let y = 10; // => 10
+let z = 7 + 8; // => 15
+let summedNums = x + y + z; // => 29
 
-console.log(summedNums)
+console.log(summedNums);
 
 //////////////////////// Strings ////////////////////////
-let str1 = "String1"
-let str2 = 'String2'
-let str3 = `String3`
+let str1 = "String1";
+let str2 = "String2";
+let str3 = `String3`;
 
-let lastName = "Stuart"
+let lastName = "Stuart";
 
-let concat = firstName + lastName
+let concat = firstName + lastName;
 
-console.log(concat)
+console.log(concat);
 
-concat = firstName + " " + lastName + '😀'
+concat = firstName + " " + lastName + "😀";
 
-console.log(concat)
+console.log(concat);
 
-console.log(`Hello, my name is ${firstName}.`)
+console.log(`Hello, my name is ${firstName}.`);
 
 //////////////////////// Getting User Input ////////////////////////
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 // reader.question("Howdy, what's your name?", function(answer) {
 //   console.log("Hi " + answer);
 // });
 
-reader.question("Enter a number to find out if it is odd or even: ", function(number) {
-  if (number % 2 === 0) {
-    console.log("Even")
-  } else {
-    console.log("Odd")
+reader.question(
+  "Enter a number to find out if it is odd or even: ",
+  function (number) {
+    if (number % 2 === 0) {
+      console.log("Even");
+    } else {
+      console.log("Odd");
+    }
   }
-});
+);
